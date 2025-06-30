51a438577b4a871fdb2d3aadb88b19d7801fce6a
Mochammad Farros Fatchur Roji <56761912+farrosfr@users.noreply.github.com>
2025-07-01 00:08:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -91,3 +91,15 @@ resetBtn.addEventListener("click", () => {
   updateCounter();
   localStorage.removeItem('tasbihCount'); // Hapus saat reset
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
